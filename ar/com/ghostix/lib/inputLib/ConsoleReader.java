edf79464a9880195b693b9897e512a8d23f9abba
Haziel <hazielescuela1@gmail.com>
2024-08-23 12:44:18 -0300

    Finished  ConsoleReader (Not tested yet)

--- ar/com/ghostix/lib/inputLib/ConsoleReader.java
+++ ar/com/ghostix/lib/inputLib/ConsoleReader.java
@@ -1,6 +1,8 @@
 package ar.com.ghostix.lib.inputLib;
 
 import java.io.InputStream;
+import java.math.BigDecimal;
+import java.math.BigInteger;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
@@ -9,22 +11,25 @@ public class ConsoleReader{
     public ConsoleReader(InputStream inputStream){
         scan = new Scanner(inputStream);
     }
+    public <T> boolean askConfirmation(T value){
+        System.out.println(STR."Estas seguro, profe? Valor introducido: \{value} S/N...");
+        String option = scan.next().toLowerCase().substring(0,1);
+        return option.equals("s");
+    }
     //String.
-    public String input(String prompt){
+    public String input(String prompt, boolean askForUserConfirmation){
         String input;
         while(true){
             System.out.print(prompt);
             input = scan.nextLine();
             input = scan.nextLine();
-            System.out.println(STR."Estas seguro, profe? Valor introducido: \{input} S/N...");
-            String option = scan.next().toLowerCase().substring(0,1);
-            if(option.equals("s")){
-                return input;
+            if(askForUserConfirmation){
+                if(askConfirmation(input)){return input;}
             }
         }
     }
     //String hasta salto de linea.
-    public String input(String prompt, boolean lineJump){
+    public String input(String prompt, boolean askForUserConfirmation, boolean lineJump){
         String input;
         while(true){
             System.out.print(prompt);
@@ -34,24 +39,20 @@ public class ConsoleReader{
             }else{
                 input = scan.next();
             }
-            System.out.println(STR."Estas seguro, profe? Valor introducido: \{input} S/N...");
-            String option = scan.next().toLowerCase().substring(0,1);
-            if(option.equals("s")){
-                return input;
+            if(askForUserConfirmation){
+                if(askConfirmation(input)){return input;}
             }
         }
 
     }
     //Integer.
-    public int input(String prompt, int input){
+    public int input(String prompt, int input, boolean askForUserConfirmation){
         while(true){
             System.out.print(prompt);
             try{
                 input = scan.nextInt();
-                System.out.println(STR."Estas seguro, profe? Valor introducido: \{input} S/N...");
-                String option = scan.next().toLowerCase().substring(0,1);
-                if(option.equals("s")){
-                    return input;
+                if(askForUserConfirmation){
+                    if(askConfirmation(input)){return input;}
                 }
             }catch(InputMismatchException error){
                 System.out.println("El valor ingresado no es un número entero. Ingrese otro...");
@@ -60,15 +61,13 @@ public class ConsoleReader{
         }
     }
     //Double.
-    public double input(String prompt, double input){
+    public double input(String prompt, double input, boolean askForUserConfirmation){
         while(true){
             System.out.print(prompt);
             try{
                 input = scan.nextDouble();
-                System.out.println(STR."Estas seguro, profe? Valor introducido: \{input} S/N...");
-                String option = scan.next().toLowerCase().substring(0,1);
-                if(option.equals("s")){
-                    return input;
+                if(askForUserConfirmation){
+                    if(askConfirmation(input)){return input;}
                 }
             }catch(InputMismatchException error){
                 System.out.println("El valor ingresado no es un número fraccionario. Ingrese otro...");
@@ -76,4 +75,110 @@ public class ConsoleReader{
             }
         }
     }
+    //Float
+    public float input(String prompt, float input, boolean askForUserConfirmation){
+        while(true){
+            System.out.print(prompt);
+            try{
+                input = scan.nextFloat();
+                if(askForUserConfirmation){
+                    if(askConfirmation(input)){return input;}
+                }
+            }catch(InputMismatchException error){
+                System.out.println("El valor ingresado no es un número fraccionario. Ingrese otro...");
+                scan.nextLine();
+            }
+        }
+    }
+    //Boolean
+    public boolean input(String prompt, Boolean input, boolean askForUserConfirmation){
+        while(true){
+            System.out.print(prompt);
+            try{
+                input = scan.nextBoolean();
+                if(askForUserConfirmation){
+                    if(askConfirmation(input)){return input;}
+                }
+            }catch(InputMismatchException error){
+                System.out.println("El valor ingresado no es un boolean. Ingrese otro...");
+                scan.nextLine();
+            }
+        }
+    }
+    //Long
+    public long input(String prompt, long input, boolean askForUserConfirmation){
+        while(true){
+            System.out.print(prompt);
+            try{
+                input = scan.nextLong();
+                if(askForUserConfirmation){
+                    if(askConfirmation(input)){return input;}
+                }
+            }catch(InputMismatchException error){
+                System.out.println("El valor ingresado no es un número. Ingrese otro...");
+                scan.nextLine();
+            }
+        }
+    }
+    //Byte
+    public byte input(String prompt, byte input, boolean askForUserConfirmation){
+        while(true){
+            System.out.print(prompt);
+            try{
+                input = scan.nextByte();
+                if(askForUserConfirmation){
+                    if(askConfirmation(input)){return input;}
+                }
+            }catch(InputMismatchException error){
+                System.out.println("El valor ingresado no es un byte. Ingrese otro...");
+                scan.nextLine();
+            }
+        }
+    }
+    //Short
+    public short input(String prompt, short input, boolean askForUserConfirmation){
+        while(true){
+            System.out.print(prompt);
+            try{
+                input = scan.nextShort();
+                if(askForUserConfirmation){
+                    if(askConfirmation(input)){return input;}
+                }
+            }catch(InputMismatchException error){
+                System.out.println("El valor ingresado no es un número fraccionario. Ingrese otro...");
+                scan.nextLine();
+            }
+        }
+    }
+    //BigDecimal
+    public BigDecimal input(String prompt, BigDecimal input, boolean askForUserConfirmation){
+        while(true){
+            System.out.print(prompt);
+            try{
+                input = scan.nextBigDecimal();
+                if(askForUserConfirmation){
+                    if(askConfirmation(input)){return input;}
+                }
+            }catch(InputMismatchException error){
+                System.out.println("El valor ingresado no es un número fraccionario. Ingrese otro...");
+                scan.nextLine();
+            }
+        }
+    }
+    //BigInteger
+    public BigInteger input(String prompt, BigInteger input, boolean askForUserConfirmation){
+        while(true){
+            System.out.print(prompt);
+            try{
+                input = scan.nextBigInteger();
+                if(askForUserConfirmation){
+                    if(askConfirmation(input)){return input;}
+                }
+            }catch(InputMismatchException error){
+                System.out.println("El valor ingresado no es un número fraccionario. Ingrese otro...");
+                scan.nextLine();
+            }
+        }
+    }
+
 }
